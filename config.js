80bdb5f425e638e36f7fc14210cf4a7386d53752
HYUK CHAN KWON <kwonssy02@gmail.com>
2016-06-02 23:19:19 +0900

    info cap

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  local: {
-    port: 8080,
-    token: '198292508:AAH9WrRTEBrL_cS8hMa3TlSZfoUHQ1Oh3RQ'
-  }
-}['local']
--- dbconnection.js
+++
@@ -1,9 +0,0 @@
-module.exports = function() { 
-	this.connection = require('mysql').createConnection({
-                host : "awsdbinstance.ctgrun1qbjn8.ap-northeast-2.rds.amazonaws.com",
-                port : 3306,
-                user : "master",
-                password : "master",
-                database : "someTar"
-	});
-};
